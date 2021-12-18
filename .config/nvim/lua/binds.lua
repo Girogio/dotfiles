13e8bd46339306f4083d4b9497ac1d3293870970
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2021-12-18 18:06:12 +0100

    Added lua vim cfg

---
+++ .config/nvim/lua/binds.lua
@@ -0,0 +1,23 @@
+-- general bindings --
+
+-- set leader
+vim.g.mapleader = ";"
+
+-- toggle spell check
+vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>set spell!<cr>", { noremap = true, silent = true })
+
+-- open startify
+vim.api.nvim_set_keymap("n", "WQ", "<cmd>w<cr><cmd>Startify<cr>", { noremap = true, silent = true})
+
+-- clear highlighting on <ESC>
+vim.api.nvim_set_keymap("n", "<esc>", "<cmd>nohl<cr>", { noremap = true, silent = true })
+
+-- move between buffers
+vim.api.nvim_set_keymap("n", "<leader>", "<cmd>bprev<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>.", "<cmd>bnext<cr>", { noremap = true, silent = true })
+
+-- terminal bindings
+vim.api.nvim_set_keymap("n", "<leader>tb", "<cmd>term<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>th", "<cmd>split term://$SHELL<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tv", "<cmd>vsplit term://$SHELL<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<esc>", "<C-\\><C-n>", { noremap = true, silent = true })
